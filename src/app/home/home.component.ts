b9c51a9dc5bc1b966f30357a6044347a1e547b90
NiklasNordgren <N_nordgren@hotmail.com>
2024-04-28 21:45:17 +0200

    turned off touch gestures for mat tooltip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipModule } from '@angular/material/tooltip';
 import { Observable, map } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  providers: [
+    { provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: { touchGestures: 'off' } }
+  ],
   imports: [
     CommonModule,
     MatListModule,
